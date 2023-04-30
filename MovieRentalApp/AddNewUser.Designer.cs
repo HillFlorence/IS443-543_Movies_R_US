2e524fc44baa9bd56128112ac8ca9119834e10fe
BikalNepal <bikalnepalsgs4@gmail.com>
2023-04-30 16:43:26 -0500

    add new user

--- MovieRentalApp/AddNewUser.Designer.cs
+++ MovieRentalApp/AddNewUser.Designer.cs
@@ -30,133 +30,151 @@
         {
             this.labelLastName = new System.Windows.Forms.Label();
             this.labelFirstName = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textboxFirstName = new System.Windows.Forms.TextBox();
             this.buttonCreateMyAccount = new System.Windows.Forms.Button();
             this.labelStreetAddress = new System.Windows.Forms.Label();
             this.labelCity = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textboxCity = new System.Windows.Forms.TextBox();
+            this.textboxStreetAddress = new System.Windows.Forms.TextBox();
+            this.textboxLastName = new System.Windows.Forms.TextBox();
+            this.textboxState = new System.Windows.Forms.TextBox();
+            this.textboxEmail = new System.Windows.Forms.TextBox();
             this.labelEmail = new System.Windows.Forms.Label();
             this.buttonClose3 = new System.Windows.Forms.Button();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.labelSlogan3 = new System.Windows.Forms.Label();
+            this.textboxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.textboxUsername = new System.Windows.Forms.TextBox();
+            this.textboxPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // labelLastName
             // 
             this.labelLastName.AutoSize = true;
-            this.labelLastName.Location = new System.Drawing.Point(280, 124);
+            this.labelLastName.Location = new System.Drawing.Point(480, 72);
             this.labelLastName.Name = "labelLastName";
-            this.labelLastName.Size = new System.Drawing.Size(90, 20);
+            this.labelLastName.Size = new System.Drawing.Size(80, 17);
             this.labelLastName.TabIndex = 0;
             this.labelLastName.Text = "Last Name:";
             // 
             // labelFirstName
             // 
             this.labelFirstName.AutoSize = true;
-            this.labelFirstName.Location = new System.Drawing.Point(280, 76);
+            this.labelFirstName.Location = new System.Drawing.Point(480, 27);
             this.labelFirstName.Name = "labelFirstName";
-            this.labelFirstName.Size = new System.Drawing.Size(90, 20);
+            this.labelFirstName.Size = new System.Drawing.Size(80, 17);
             this.labelFirstName.TabIndex = 1;
             this.labelFirstName.Text = "First Name:";
             // 
-            // textBox1
+            // textboxFirstName
             // 
-            this.textBox1.Location = new System.Drawing.Point(445, 76);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(183, 26);
-            this.textBox1.TabIndex = 2;
+            this.textboxFirstName.Location = new System.Drawing.Point(577, 24);
+            this.textboxFirstName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textboxFirstName.Name = "textboxFirstName";
+            this.textboxFirstName.Size = new System.Drawing.Size(163, 22);
+            this.textboxFirstName.TabIndex = 2;
             // 
             // buttonCreateMyAccount
             // 
-            this.buttonCreateMyAccount.Location = new System.Drawing.Point(464, 361);
+            this.buttonCreateMyAccount.Location = new System.Drawing.Point(701, 282);
+            this.buttonCreateMyAccount.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonCreateMyAccount.Name = "buttonCreateMyAccount";
-            this.buttonCreateMyAccount.Size = new System.Drawing.Size(144, 52);
+            this.buttonCreateMyAccount.Size = new System.Drawing.Size(128, 42);
             this.buttonCreateMyAccount.TabIndex = 7;
             this.buttonCreateMyAccount.Text = "Create My Account";
             this.buttonCreateMyAccount.UseVisualStyleBackColor = true;
+            this.buttonCreateMyAccount.Click += new System.EventHandler(this.buttonCreateMyAccount_Click);
             // 
             // labelStreetAddress
             // 
             this.labelStreetAddress.AutoSize = true;
-            this.labelStreetAddress.Location = new System.Drawing.Point(280, 172);
+            this.labelStreetAddress.Location = new System.Drawing.Point(454, 124);
             this.labelStreetAddress.Name = "labelStreetAddress";
-            this.labelStreetAddress.Size = new System.Drawing.Size(120, 20);
+            this.labelStreetAddress.Size = new System.Drawing.Size(106, 17);
             this.labelStreetAddress.TabIndex = 8;
             this.labelStreetAddress.Text = "Street Address:";
             // 
             // labelCity
             // 
             this.labelCity.AutoSize = true;
-            this.labelCity.Location = new System.Drawing.Point(331, 212);
+            this.labelCity.Location = new System.Drawing.Point(503, 183);
             this.labelCity.Name = "labelCity";
-            this.labelCity.Size = new System.Drawing.Size(39, 20);
+            this.labelCity.Size = new System.Drawing.Size(35, 17);
             this.labelCity.TabIndex = 9;
             this.labelCity.Text = "City:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(318, 257);
+            this.label3.Location = new System.Drawing.Point(503, 244);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.Size = new System.Drawing.Size(45, 17);
             this.label3.TabIndex = 10;
             this.label3.Text = "State:";
             // 
-            // textBox2
+            // textboxCity
             // 
-            this.textBox2.Location = new System.Drawing.Point(445, 212);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(183, 26);
-            this.textBox2.TabIndex = 11;
+            this.textboxCity.Location = new System.Drawing.Point(577, 183);
+            this.textboxCity.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textboxCity.Name = "textboxCity";
+            this.textboxCity.Size = new System.Drawing.Size(163, 22);
+            this.textboxCity.TabIndex = 11;
             // 
-            // textBox3
+            // textboxStreetAddress
             // 
-            this.textBox3.Location = new System.Drawing.Point(445, 172);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(183, 26);
-            this.textBox3.TabIndex = 12;
+            this.textboxStreetAddress.Location = new System.Drawing.Point(577, 124);
+            this.textboxStreetAddress.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textboxStreetAddress.Name = "textboxStreetAddress";
+            this.textboxStreetAddress.Size = new System.Drawing.Size(163, 22);
+            this.textboxStreetAddress.TabIndex = 12;
             // 
-            // textBox4
+            // textboxLastName
             // 
-            this.textBox4.Location = new System.Drawing.Point(445, 124);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(183, 26);
-            this.textBox4.TabIndex = 13;
+            this.textboxLastName.Location = new System.Drawing.Point(577, 72);
+            this.textboxLastName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textboxLastName.Name = "textboxLastName";
+            this.textboxLastName.Size = new System.Drawing.Size(163, 22);
+            this.textboxLastName.TabIndex = 13;
+            this.textboxLastName.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
-            // textBox5
+            // textboxState
             // 
-            this.textBox5.Location = new System.Drawing.Point(445, 257);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(183, 26);
-            this.textBox5.TabIndex = 14;
+            this.textboxState.Location = new System.Drawing.Point(577, 239);
+            this.textboxState.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textboxState.Name = "textboxState";
+            this.textboxState.Size = new System.Drawing.Size(163, 22);
+            this.textboxState.TabIndex = 14;
             // 
-            // textBox6
+            // textboxEmail
             // 
-            this.textBox6.Location = new System.Drawing.Point(445, 309);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(183, 26);
-            this.textBox6.TabIndex = 15;
+            this.textboxEmail.Location = new System.Drawing.Point(895, 62);
+            this.textboxEmail.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textboxEmail.Name = "textboxEmail";
+            this.textboxEmail.Size = new System.Drawing.Size(163, 22);
+            this.textboxEmail.TabIndex = 15;
             // 
             // labelEmail
             // 
             this.labelEmail.AutoSize = true;
-            this.labelEmail.Location = new System.Drawing.Point(318, 312);
+            this.labelEmail.Location = new System.Drawing.Point(814, 62);
             this.labelEmail.Name = "labelEmail";
-            this.labelEmail.Size = new System.Drawing.Size(52, 20);
+            this.labelEmail.Size = new System.Drawing.Size(46, 17);
             this.labelEmail.TabIndex = 16;
             this.labelEmail.Text = "Email:";
             // 
             // buttonClose3
             // 
-            this.buttonClose3.Location = new System.Drawing.Point(565, 454);
+            this.buttonClose3.Location = new System.Drawing.Point(1002, 11);
+            this.buttonClose3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonClose3.Name = "buttonClose3";
-            this.buttonClose3.Size = new System.Drawing.Size(63, 28);
+            this.buttonClose3.Size = new System.Drawing.Size(56, 22);
             this.buttonClose3.TabIndex = 17;
             this.buttonClose3.Text = "Close";
             this.buttonClose3.UseVisualStyleBackColor = true;
@@ -165,7 +183,8 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::MovieRentalApp.Properties.Resources.movie;
-            this.pictureBox3.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox3.Location = new System.Drawing.Point(152, 24);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(225, 225);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -176,33 +195,112 @@
             // 
             this.labelSlogan3.AutoSize = true;
             this.labelSlogan3.Font = new System.Drawing.Font("Freestyle Script", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelSlogan3.Location = new System.Drawing.Point(12, 361);
+            this.labelSlogan3.Location = new System.Drawing.Point(90, 251);
             this.labelSlogan3.Name = "labelSlogan3";
-            this.labelSlogan3.Size = new System.Drawing.Size(417, 43);
+            this.labelSlogan3.Size = new System.Drawing.Size(340, 36);
             this.labelSlogan3.TabIndex = 19;
             this.labelSlogan3.Text = "Ssh ... The Movie is About to Start!";
             // 
+            // textboxPhoneNumber
+            // 
+            this.textboxPhoneNumber.AccessibleDescription = "PhoneNumber";
+            this.textboxPhoneNumber.Location = new System.Drawing.Point(895, 129);
+            this.textboxPhoneNumber.Name = "textboxPhoneNumber";
+            this.textboxPhoneNumber.Size = new System.Drawing.Size(163, 22);
+            this.textboxPhoneNumber.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(762, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Phone Number:";
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Location = new System.Drawing.Point(792, 188);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(77, 17);
+            this.usernameLabel.TabIndex = 22;
+            this.usernameLabel.Text = "Username:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(796, 244);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(73, 17);
+            this.passwordLabel.TabIndex = 23;
+            this.passwordLabel.Text = "Password:";
+            // 
+            // textboxUsername
+            // 
+            this.textboxUsername.Location = new System.Drawing.Point(895, 188);
+            this.textboxUsername.Name = "textboxUsername";
+            this.textboxUsername.Size = new System.Drawing.Size(163, 22);
+            this.textboxUsername.TabIndex = 24;
+            // 
+            // textboxPassword
+            // 
+            this.textboxPassword.Location = new System.Drawing.Point(895, 244);
+            this.textboxPassword.Name = "textboxPassword";
+            this.textboxPassword.PasswordChar = '*';
+            this.textboxPassword.Size = new System.Drawing.Size(163, 22);
+            this.textboxPassword.TabIndex = 25;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(725, 358);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 26;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(647, 338);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(243, 17);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Already have an account? Click login!";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // AddNewUser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(654, 516);
+            this.ClientSize = new System.Drawing.Size(1090, 459);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textboxPassword);
+            this.Controls.Add(this.textboxUsername);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.usernameLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textboxPhoneNumber);
             this.Controls.Add(this.labelSlogan3);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.buttonClose3);
             this.Controls.Add(this.labelEmail);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textboxEmail);
+            this.Controls.Add(this.textboxState);
+            this.Controls.Add(this.textboxLastName);
+            this.Controls.Add(this.textboxStreetAddress);
+            this.Controls.Add(this.textboxCity);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.labelCity);
             this.Controls.Add(this.labelStreetAddress);
             this.Controls.Add(this.buttonCreateMyAccount);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textboxFirstName);
             this.Controls.Add(this.labelFirstName);
             this.Controls.Add(this.labelLastName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AddNewUser";
             this.Text = "Add New User - MoviesR-U-S (c) 2023";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -215,19 +313,27 @@
 
         private System.Windows.Forms.Label labelLastName;
         private System.Windows.Forms.Label labelFirstName;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textboxFirstName;
         private System.Windows.Forms.Button buttonCreateMyAccount;
         private System.Windows.Forms.Label labelStreetAddress;
         private System.Windows.Forms.Label labelCity;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textboxCity;
+        private System.Windows.Forms.TextBox textboxStreetAddress;
+        private System.Windows.Forms.TextBox textboxLastName;
+        private System.Windows.Forms.TextBox textboxState;
+        private System.Windows.Forms.TextBox textboxEmail;
         private System.Windows.Forms.Label labelEmail;
         private System.Windows.Forms.Button buttonClose3;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label labelSlogan3;
+        private System.Windows.Forms.TextBox textboxPhoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox textboxUsername;
+        private System.Windows.Forms.TextBox textboxPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label2;
     }
 }
--- MovieRentalApp/AddNewUser.cs
+++ MovieRentalApp/AddNewUser.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -21,6 +22,55 @@ namespace MovieRentalApp
         {
             //Close the form
             this.Close();
+        }
+
+        private void buttonCreateMyAccount_Click(object sender, EventArgs e)
+        {
+            string conString = Properties.Settings.Default.MovieDbConnectionString;
+            SqlConnection connection = new SqlConnection(conString);
+            connection.Open();
+            string insertQuery = "INSERT INTO customer(username, password, email, phone_number, street_address, city, state, FirstName, LastName) VALUES (@username, @password, @email, @phone_number, @street_address, @city, @state, @FirstName, @LastName)";
+            SqlCommand command = new SqlCommand(insertQuery, connection);
+            command.Parameters.AddWithValue("@username", textboxUsername.Text);
+            command.Parameters.AddWithValue("@password", textboxPassword.Text);
+            command.Parameters.AddWithValue("@email", textboxEmail.Text);
+            command.Parameters.AddWithValue("@phone_number", textboxPhoneNumber.Text);
+            command.Parameters.AddWithValue("@street_address", textboxStreetAddress.Text);
+            command.Parameters.AddWithValue("@city", textboxCity.Text);
+            command.Parameters.AddWithValue("@state", textboxState.Text);
+            command.Parameters.AddWithValue("@FirstName", textboxFirstName.Text);
+            command.Parameters.AddWithValue("@LastName", textboxLastName.Text);
+            command.ExecuteNonQuery();
+            connection.Close();
+            textboxUsername.Clear();
+            textboxPassword.Clear();
+            textboxEmail.Clear();
+            textboxPhoneNumber.Clear();
+            textboxStreetAddress.Clear();
+            textboxCity.Clear();
+            textboxState.Clear();
+            textboxFirstName.Clear();
+            textboxLastName.Clear();
+
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            AddNewUser addNewUser = new AddNewUser();
+            addNewUser.Close();
+            login.Show();
         }
     }
 }
